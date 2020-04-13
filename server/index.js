6d25d6f448af457cb8e6c6c6329bb0c654735b9a
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-14 01:38:21 +0200

    sagws

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
