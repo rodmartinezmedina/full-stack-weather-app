f9c7377b477d6f4d7ce8d9458eaf7c5adc9a0352
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-14 18:51:17 +0200

    install and set postgresql and pgadmin 4 on my computer set pool query and end on index.js on server folder

---
+++ server/database/index.js
@@ -0,0 +1,44 @@
+var { Pool } = require('pg');
+
+
+// DATABASE_URL=postgres://{user}:{password}@{hostname}:{port}/{database-name} <== postgreSQL adress structure.
+const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://rodrigo_postgresql:Nuncamiresatras83!@localhost:5432/weather-db';
+const SSL = process.env.NODE_ENV === 'production'; // <== if we are in production it evaluates to 'true'. Otherwise to 'false'.
+
+class Database {
+  constructor() {
+    this._pool = new Pool({
+      connectionString: CONNECTION_STRING,
+      ssl: SSL
+    });
+
+    this._pool.on('error', (err, client) => {
+      console.error('Unexpected error on Idle PostgreSQL client.', err);
+      process.exit(-1);
+    })      
+  }
+
+  query() {
+    this._pool.connect((err, client, done) => {
+      if (err) throw err;
+      const params = args.length === 2 ? args[0] : [];
+      const callback = args.length === 1 ? args[0] : args [1];
+      
+      client.query(query, params, (err, res) => {
+        done();
+        if (err) {
+          console.log(err.stack);
+          return callback({ error: 'Database error.' }, null);
+        }
+        callback({}, res.rows);
+      });
+    });
+  }
+
+  end() {
+
+  }
+}
+
+
+module.exports = new Database();
--- server/index.js
+++ server/index.js
@@ -4,3 +4,15 @@ const bodyParser = require('body-parser');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
+
+const app= express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!!`);
+});
+
+module.exports = app;
