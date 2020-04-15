8c4c5f9db02429382f976dc901bbae049de32816
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-15 02:08:37 +0200

    API connected can retrieve data from weather api

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,11 +1,11 @@
 const request = require('request-promise');
 
-const API_KEY = "0310a08011b70cc7fea7200e55f868e7";
+const API_KEY = `7f50425f6786422f2bd9b71c1375e313`;
 
 class Weather {
   static retrieveByCity (city, callback) {
     request({
-      uri: `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units-metric`,   // <= where the data is exracted from
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units-metric`,   // <= where the data is exracted from
       json: true
     })
     .then(function (res) {
