3a05fcbd607ab9cac0770d76d3d21db32e9bed6d
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-16 14:25:56 +0200

    trying to fix deployment

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  });
+  })
 }
 
 
